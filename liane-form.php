735ca2f341f27806f817a00ce8382a9d2b173c89
Miguel Peixe <miguel@peixe.co>
2021-01-28 19:54:32 -0300

    add settings page

--- liane-form.php
+++ liane-form.php
@@ -4,44 +4,148 @@
  * Plugin Name: Liane Form
  * Plugin URI: https://github.com/lianetoolkit/liane-form-wpplugin
  * Description: Embed Liane Forms
- * Version: 0.1.1
+ * Version: 0.1.2
  * Author: Instituto Update
  * Author URI: https://institutoupdate.org.br
  */
 
 class Liane_Form {
   function __construct() {
+    add_action( 'admin_menu', array($this, 'admin_menu' ) );
+    add_action( 'admin_init', array($this, 'register_settings' ) );
     add_action( 'wp_footer', array( $this, 'wp_footer' ) );
     add_shortcode( 'liane_form' , array( $this, 'shortcode' ) );
   }
-  function wp_footer() {
-    wp_enqueue_script( 'liane-form', plugin_dir_url(__FILE__) . 'liane-form.js', array(), '0.1.1' );
+  function admin_menu() {
+    add_options_page(
+      'Liane Form Settings',
+      'Liane Form',
+      'manage_options',
+      'liane_form',
+      array( $this, 'settings_page' )
+    );
   }
-  function shortcode($atts) {
-    $a = shortcode_atts( array(
-      'url' => '',
-      'id' => '',
-      'allow_non_secure' => "0"
-    ), $atts );
+  function settings_page() {
+    ?>
+    <div class="wrap">
+      <h1>Liane Form</h1>
+      <form action="options.php" method="post">
+        <?php
+        settings_fields( 'liane_form_settings' );
+        do_settings_sections( 'liane_form' );
+        ?>
+        <input
+          type="submit"
+          name="submit"
+          class="button button-primary"
+          value="<?php esc_attr_e( 'Save' ); ?>"
+        />
+      </form>
+    </div>
+    <?php
+  }
+  function register_settings() {
+    register_setting(
+      'liane_form_settings',
+      'liane_form_settings',
+      array( $this, 'validate_settings' )
+    );
 
-    $url = filter_var($a['url'], FILTER_VALIDATE_URL);
-    $allow_non_secure = filter_var($a['allow_non_secure'], FILTER_VALIDATE_BOOLEAN);
+    add_settings_section(
+      'default',
+      '',
+      array( $this, 'render_section' ),
+      'liane_form'
+    );
+
+    add_settings_field(
+      'server',
+      'Server',
+      array( $this, 'render_server_field' ),
+      'liane_form',
+      'default'
+    );
+    add_settings_field(
+      'id',
+      'Campaign ID',
+      array( $this, 'render_id_field' ),
+      'liane_form',
+      'default'
+    );
+    add_settings_field(
+      'shortcode',
+      'Shortcode',
+      array($this, 'render_shortcode_field'),
+      'liane_form',
+      'default'
+    );
+  }
+  function render_section() {
+    echo "<p>Setup your Liane server and campaign ID to embed forms using the shortcode.</p>";
+  }
+  function render_text_field($name) {
+    $options = get_option( 'liane_form_settings' );
+    printf(
+      '<input type="text" name="%s" value="%s" />',
+      esc_attr( 'liane_form_settings[' . $name . ']' ),
+      esc_attr( $options[$name] )
+    );
+  }
+  function render_server_field() {
+    return $this->render_text_field('server');
+  }
+  function render_id_field() {
+    return $this->render_text_field('id');
+  }
+  function render_shortcode_field() {
+    echo '<p><code>[liane_form]</code></p>';
+    echo '<p class="description">Paste this shortcode anywhere on a page or post</p>';
+  }
+  function validate_settings($input) {
+    $output = array();
+
+    // Sanitize text field
+    $server = sanitize_text_field($input["server"]);
+    $id = sanitize_text_field($input["id"]);
+
+    // Validate server url
+    $server = filter_var($server, FILTER_VALIDATE_URL);
+
+    if(!$server) {
+      add_settings_error('liane_form', '400', 'You must provide a valid URL for the server', 'error');
+    } else {
+      $output['server'] = $server;
+    }
+
+    // Validate ID
+    if(strlen($id) > 17 || !preg_match('/^[\w\d]{17}$/', $id)) {
+      add_settings_error('liane_form', '400', 'You must provide a valid Campaign ID', 'error');
+    } else {
+      $output['id'] = $id;
+    }
+
+    return $output;
+  }
+  function shortcode() {
+
+    $options = get_option( 'liane_form_settings' );
 
     // Validate non-empty required atts
-    if(!$url || !$a['id']) {
+    if(!$options['server'] || !$options['id']) {
       return "Invalid form parameters.";
     }
 
-    // Validate ID match
-    if(strlen($a['id']) > 17 || !preg_match('/^[\w\d]{17}$/', $a['id'])) {
-      return "Invalid form parameters.";
-    }
+    // Non secure verification
+    $allow_non_secure = true;
+    if(substr($options['server'], 0, 5) == 'https')
+      $allow_non_secure = false;
+
     ob_start();
     ?>
     <div
       class="liane-form"
-      data-url="<?php echo $url; ?>"
-      data-campaignId="<?php echo $a['id']; ?>"
+      data-url="<?php echo $options['server']; ?>"
+      data-campaignId="<?php echo $options['id']; ?>"
       <?php if($allow_non_secure) : ?>
         data-allowNonSecure="true"
       <?php endif; ?>
@@ -49,6 +153,9 @@ class Liane_Form {
     </div>
     <?php
     return ob_get_clean();
+  }
+  function wp_footer() {
+    wp_enqueue_script( 'liane-form', plugin_dir_url(__FILE__) . 'liane-form.js', array(), '0.1.1' );
   }
 }
 
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://institutoupdate.org.br
 Tags: liane, form, shortcode
 Requires at least: 4.6
 Tested up to: 5.6
-Stable tag: 0.1.1
+Stable tag: 0.1.2
 Requires PHP: 7.0
 License: MIT
 License URI: https://github.com/lianetoolkit/liane-form-wpplugin/blob/main/LICENSE
@@ -13,13 +13,24 @@ Shortcode for embedding Liane forms
 
 == Description ==
 
-Use a simple shortcode to embed your campaign form into your WordPress site!
+This plugin is for exclusive use with [Liane](https://liane.voto), an open source software for political innovation.
+
+Use a simple shortcode to embed your campaign form into your WordPress site.
 
 = Usage =
 
-[liane_form url="https://liane-server.com" id="your-campaign-id"]
+* On your Dashboard, go to Settings > Liane
+* Fill the form with proper information provided in your Liane dashboard
+* Use `[liane_form]` shortcode in any post or page to embed your form!
 
 == Changelog ==
+
+= 0.1.2 =
+* Add settings page for plugin configuration
+* Remove attributes from shortcode
+
+= 0.1.1 =
+* Add shortcode attributes validation
 
 = 0.1.0 =
 * First release
